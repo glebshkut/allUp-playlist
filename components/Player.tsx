f4627df9dd5a1bbdcf0ff85eeeeed0e1b05e9a2a
Gleb <gleb.shkut@gmail.com>
2023-05-02 16:51:20 -0700

    add a spinner

--- components/Player.tsx
+++ components/Player.tsx
@@ -1,6 +1,8 @@
 import ReactPlayer from 'react-player'
 import { videoItem } from "@/pages/index";
 import { useMediaQuery } from 'react-responsive'
+import { useEffect, useState } from "react";
+import ClipLoader from "react-spinners/ClipLoader";
 
 interface PlayerProps {
   video: videoItem;
@@ -9,6 +11,11 @@ interface PlayerProps {
 
 const Player: React.FC<PlayerProps> = ({ video, fullWidth }) => {
   const isMobile = useMediaQuery({ maxWidth: 768 })
+  const [isLoading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setLoading(true);
+  }, [video])
 
   const openFull = () => {
     if (isMobile) {
@@ -26,6 +33,8 @@ const Player: React.FC<PlayerProps> = ({ video, fullWidth }) => {
     <div className="h-full w-full flex justify-center items-center">
       <div className={`${openFull() ? 'h-full w-full' : 'relative flex justify-center'}`}>
         <ReactPlayer
+          onPlay={() => setLoading(false)}
+          style={{ display: isLoading ? 'none' : 'block' }}
           url={video.src}
           playing={true}
           muted={true}
@@ -33,6 +42,9 @@ const Player: React.FC<PlayerProps> = ({ video, fullWidth }) => {
           width={openFull() ? "100%" : "70%"}
           height={openFull() ? "100%" : "70%"}
         />
+        {isLoading && <div>
+          <ClipLoader size={50} color="#86EFAC" />
+        </div>}
       </div>
     </div>
   )
